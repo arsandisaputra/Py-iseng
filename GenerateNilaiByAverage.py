04cb638effc5814ed99c7371a735a78a0e5bacc5
Arsandi Saputra <nyankoder@gmail.com>
2023-12-07 15:53:16 +0700

    progress

--- GenerateNilaiByAverage.py
+++ GenerateNilaiByAverage.py
@@ -1,11 +1,12 @@
+import sys
 import openpyxl
 import win32com.client
 import subprocess
 
 import random
 
-batasBawah = 73
-batasAtas = 97
+batasBawah = 79
+batasAtas = 95
 
 P1 = 0
 P2 = 0
@@ -20,6 +21,7 @@ K1 = 0
 K2 = 0
 K3 = 0
 K4 = 0
+
 
 def open_excel_file(file_path):
     try:
@@ -39,6 +41,7 @@ def close_excel_file(file_path):
     except Exception as e:
         print(f'Error: {e}')
 
+
 def acakByMean(mean):
     var1 = mean - batasBawah
     var2 = batasAtas - mean
@@ -47,26 +50,108 @@ def acakByMean(mean):
     angka = random.randint(-1 * randomVar, randomVar)
     return angka
 
+
 def GenerateNilai(mean):
     # Ubah nilai sel
-    RPH = mean + acakByMean(mean)
-    PTS = mean
-    PAS = mean
-    
-    P1 = 0
-    P2 = 0
-    P3 = 0
-    P4 = 0
-    
-    NilaiPengetahuan = mean
+    while True:
+        acak = acakByMean(mean)
+        # print(acak)
+        RPH = mean + acak
+        if RPH >= batasBawah and RPH <= batasAtas:
+            break
 
-    K1 = 0
-    K2 = 0
-    K3 = 0
-    K4 = 0
+    print('RPH', RPH)
+
+    sisa = (mean * 4) - ((RPH * 2) + (mean*2))
+    # print(sisa)
+
+    while True:
+        randPosisi = random.randint(0, 1)
+        # print('pos', randPosisi)
+        randNext = acakByMean(mean) / 2
+        if randPosisi == 0:
+            PTS = mean + randNext + (sisa / 2)
+            PAS = mean - randNext + (sisa / 2)
+        else:
+            PTS = mean - randNext + (sisa / 2)
+            PAS = mean + randNext + (sisa / 2)
+
+        if PTS >= batasBawah and PTS <= batasAtas and PAS >= batasBawah and PAS <= batasAtas:
+            break
+
+    print('PTS', int(PTS), 'PAS', int(PAS))
+
+    print('Mean awal:', mean, ' Mean kemudian:', int(((RPH*2) + PTS + PAS)/4))
+
+    P = [RPH, RPH, RPH, RPH]
+    sisaP = 0
+    pos = [0, 1, 2, 3]
+    randPosisi = random.randint(0, len(pos)-1)
+    # print(randPosisi)
+    # print(pos[randPosisi])
+    index = pos[randPosisi]
+
+    while True:
+        acak = acakByMean(RPH)
+        # print(acak)
+        val = RPH + acak
+        if val >= batasBawah and val <= batasAtas:
+            P[index] = val
+            sisaP += RPH - val
+            break
+
+    pos.pop(randPosisi)
+    # print(pos)
+
+    randPosisi = random.randint(0, len(pos)-1)
+    # print(randPosisi)
+    # print(pos[randPosisi])
+    index = pos[randPosisi]
+    while True:
+        acak = acakByMean(RPH)
+        # print(acak)
+        val = RPH + acak
+        if val >= batasBawah and val <= batasAtas:
+            P[index] = val
+            sisaP += RPH - val
+            break
+
+    pos.pop(randPosisi)
+    print(pos)
+
+    index1 = pos[0]
+    index2 = pos[1]
+    while True:
+        randPosisi = random.randint(0, 1)
+        # print('pos', randPosisi)
+        randNext = acakByMean(RPH) / 2
+
+        if randPosisi == 0:
+            P[index1] = RPH + randNext + (sisaP / 2)
+            P[index2] = RPH - randNext + (sisaP / 2)
+        else:
+            P[index1] = RPH - randNext + (sisaP / 2)
+            P[index2] = RPH + randNext + (sisaP / 2)
+
+        if P[index1] >= batasBawah and P[index1] <= batasAtas and P[index2] >= batasBawah and P[index2] <= batasAtas:
+            break
+    P[index1] = int(P[index1])
+    P[index2] = int(P[index2])
+    print(P)
+    print('mean RPH', sum(P) / len(P))
+    # P1 = 0
+    # P2 = 0
+    # P3 = 0
+    # P4 = 0
+
+    # NilaiPengetahuan = mean
+
+    # K1 = 0
+    # K2 = 0
+    # K3 = 0
+    # K4 = 0
 
 
-    
 def save_to_excel():
     # Buka file Excel
     excel_file = 'data_excel.xlsx'
@@ -90,12 +175,11 @@ def save_to_excel():
     # Simpan perubahan
     workbook.save(excel_file)
 
-    print('Perubahan file', excel_file ,'telah disimpan.')
+    print('Perubahan file', excel_file, 'telah disimpan.')
 
 
 # script.py
 
-import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
